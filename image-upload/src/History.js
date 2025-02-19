4a37b97fcbf3fd7ed1df55845c59984b8fe50d26
winston <winstonyi02@gmail.com>
2025-02-19 15:13:47 -0800

    Added redirecting message when logging in

--- image-upload/src/History.js
+++ image-upload/src/History.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
 
 const History = ({isLoggedIn}) => {
-    const auth = useAuth(); // Get auth state from Cognito
-    console.log(auth);
+    const auth = useAuth(); 
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+            auth.signinRedirect();
+        }
+    }, [auth.isAuthenticated]);
+
+    if (!auth.isAuthenticated) {
+        return <div>Redirecting...</div>; // Optional loading message
+    }
     return <div>Testing Routes</div>
 }
 
